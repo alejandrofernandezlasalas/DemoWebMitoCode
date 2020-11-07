122232990ac26fefc04c45d32d0bea6e7d45cb86
aflasalas <alejandrofernandezlasalas20@gmail.com>
2020-11-07 19:58:34 -0300

    Primer programa en la web(andando)

--- src/main/java/com/mitocode/controller/DemoController.java
+++ src/main/java/com/mitocode/controller/DemoController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DemoController {
 
-	@GetMapping("/greeting")
+	@GetMapping("/saludo")
 	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
 		
 		//Al modelo le estamos agregando un atributo
 		model.addAttribute("name", name);
 		
 		//Hace mencion a la pagina en la que quieres manejar
-		return "gretting";
+		return "greeting";
 	}
 }
